6ebaf317c5230392d895ee007abad076dcafd743
Pradeep-patel06 <pradeeppatel052006@gmail.com>
2026-06-02 01:13:24 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,52 +8,32 @@ dotenv.config();
 const app = express();
 
 
-
 app.use(cors());
-
 app.use(express.json());
 
 
-
-app.use(
-  "/api/auth",
-  require("./routes/authRoutes")
-);
-
-app.use(
-  "/api/workouts",
-  require("./routes/workoutRoutes")
-);
-
-
-
-app.use(
-  "/api/payment",
-  require("./routes/paymentRoutes")
-);
-
-
-
-mongoose.connect(process.env.MONGO_URI)
-
-.then(() => {
-
-  console.log("MongoDB Connected");
-
-})
-
-.catch((err) => {
-
-  console.log(err);
-
+app.get("/", (req, res) => {
+  res.send("Fitness Tracker Backend is running 🚀");
 });
 
 
+app.use("/api/auth", require("./routes/authRoutes"));
+app.use("/api/workouts", require("./routes/workoutRoutes"));
+app.use("/api/payment", require("./routes/paymentRoutes"));
 
-app.listen(5000, () => {
 
-  console.log(
-    "Server running on port 5000"
-  );
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB Error:", err);
+  });
 
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
